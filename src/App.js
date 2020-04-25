564b89c55ef4179706a60864cd689a6a57d690d6
harshavardhan <harshavardhan@panainfotech.com>
2020-04-25 17:31:16 +0530

    Material UI buttons

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import InputComponent from './InputComponent';
+function App() {
+  return (
+    <React.Fragment>
+      <div className='container'>
+        <InputComponent></InputComponent>
+      </div>
+    </React.Fragment>
+  );
+}
+
+export default App;
